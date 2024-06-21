5e5305d2782c148ddf6bd160bd706145079b9640
gabrielfel1x <gabrielfelxx@gmail.com>
2024-06-20 22:52:41 -0300

    change text width

--- src/organisms/DivSup/index.jsx
+++ src/organisms/DivSup/index.jsx
@@ -1,18 +1,20 @@
 export default function DivSup() {
-    return (
-        <div className=" h-48 my-7 flex mx-28 flex-row ">
-            <div className="flex flex-col justify-center basis-1/2 text-purple font-sans font-semibold text-5xl">
-                <div className="flex flex-col justify-center">
-                    <div className="flex justify-center">Sua jornada de</div>
-                    <div className="text-orange flex justify-center">conhecimento</div>
-                </div>
-                <div className="flex justify-center">
-                    começa &nbsp;<span className="text-orange">agora</span>!
-                </div>
-            </div>
-            <div className="text-gray pt-5 basis-1/2 text-xl px-64 text-right">
-                Realize suas avaliações de maneira <u >digital</u> e <u>intuitiva</u>. Com o <b>EXAME</b>, você terá acesso a uma plataforma <u>prática</u> para estudar.
-            </div>
+  return (
+    <div className=" h-48 my-7 flex mx-28 flex-row ">
+      <div className="flex flex-col justify-center basis-1/2 text-purple font-sans font-semibold text-5xl">
+        <div className="flex flex-col justify-center">
+          <div className="flex justify-center">Sua jornada de</div>
+          <div className="text-orange flex justify-center">conhecimento</div>
         </div>
-    )
+        <div className="flex justify-center">
+          começa &nbsp;<span className="text-orange">agora</span>!
+        </div>
+      </div>
+      <div className="text-gray pt-5 basis-8/12 text-xl text-right px-4 sm:px-8 md:px-16 lg:px-32 xl:px-64 max-w-full">
+        Realize suas avaliações de maneira <u>digital</u> e <u>intuitiva</u>.
+        Com o <b>EXAME</b>, você terá acesso a uma plataforma <u>prática</u>{" "}
+        para estudar.
+      </div>
+    </div>
+  );
 }
